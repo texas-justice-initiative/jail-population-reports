c226a271788129f3a9f43f945e1279c7f775ddfa
ayota <projects@elaineayo.com>
2022-01-31 18:31:09 -0500

    add serious incident tables

---
+++ warehouse/models/prep/serious_incidents_data_date.sql
@@ -0,0 +1,5 @@
+SELECT
+    TO_DATE(CONCAT(SPLIT_PART("8", ' ', 1), SPLIT_PART("8", ' ', 2)), 'Month YYYY')
+    , processed_at
+FROM {{ source('tcjs_jail_population_report', 'serious_incidents') }}
+WHERE "8" LIKE '%Serious Incident%'
